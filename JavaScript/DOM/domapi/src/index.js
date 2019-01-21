1621f234c9a33546b1db4013077f5f5cb47cdbe4
Samson Monballiu <samsonmonballiu@hotmail.com>
2019-01-21 11:14:21 +0100

    DOM React App

---
+++ JavaScript/DOM/domapi/src/index.js
@@ -0,0 +1,8 @@
+require('./index.css'); /*adds content of index.css in JS */
+
+
+// const para = document.createElement("PARAGRAPH");
+// para.appendChild(document.createTextNode("hello world"));
+// document.getElementById("root").appendChild(para);
+
+document.body.appendChild(document.createTextNode("hello world"));
